33c48a7b5ffd522f76189a4957eb7e8a2ba7e4ee
codesungrape <miss.s.rai@outlook.com>
2025-03-29 19:57:15 +0000

    - Added Cube, Sphere, Torus and TorusKnot shape components with animations as well. - Changed background to see the wireframe effect on sphere - Added useState to change the colour of teh sphere as user hovers over the sphere.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import { Canvas } from '@react-three/fiber';
 import { useErrorBoundary} from 'use-error-boundary';
 import Cube from './components/Cube';
 import Sphere from './components/Sphere';
+import Torus from './components/Torus';
+import TorusKnot from './components/TorusKnot';
+
 
 const App = () => {
 
   // safeguarding the canavs against WebGL context crashes
   const { ErrorBoundary, didCatch, error } = useErrorBoundary();
+
 
   
   return didCatch ? (<div>{error.message}</div>) :
@@ -16,36 +20,51 @@ const App = () => {
       <Canvas fallback={<div>Sorry no WebGL supported!</div>}>
         
         {/* Environment setup */}
-      <ambientLight intensity={0.3} />
+      <ambientLight intensity={0.4} />
       <directionalLight position={[2, 5, 5]} intensity={1} />
 
-      
-          {/* fundamental building block for rendering any building bocks. A mesh needs a germetry shape and everything within the mesh are its properties- like a container*/}
+      <group position={[0, 0, 0]}>
           <Cube 
             position={[1, 0, 0]}
             args={[1, 1, 1]}
             color={"yellow"}
           />
-          <Cube 
+          {/* <Cube 
             position={[-1, 0, 1]}
             args={[1, 1, 1]}
             color={"lightgreen"}
-          />
+          /> */}
           <Cube 
             position={[-1, 2, 0]}
             args={[1, 1, 1]}
             color={"pink"}
-          />
-          <Sphere
-            position={[2, 0, 1]}
-            args={[1, 1, 1]}
-            color={"#ff7700"}
           />
           <Cube 
             position={[0, 1, 0]}
             args={[1, 1, 1]}
             color={"lightblue"}
           />
+      </group>
+          <Sphere
+            position={[-2, 0, 1]}
+            // args={[radius, widthSegments, heightSegments]}
+            args={[1, 25, 40]}
+            isHovered={"hotpink"}
+          />
+          <Torus 
+            position={[2, 0, 0]}
+            // args={[radius, widthSegments, heightSegments]}
+            args={[0.25, 0.2, 5]}
+            color={"hotpink"}
+          />
+          <TorusKnot
+            position={[0, 3, 2]}
+            // args={[radius, widthSegments, heightSegments]}
+            args={[1, 0.1, 50, 25, 2, 5]}
+            color={"hotpink"}
+          />
+
+          
       </Canvas>
     </ErrorBoundary>
 
--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,8 +1,23 @@
-export const Cube = ({ position, side, color }) => {
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+export const Cube = ({ position, args, color }) => {
+
+    const ref = useRef();
+
+    useFrame((state, delta) => {
+        ref.current.rotation.y -= delta;
+        ref.current.rotation.x -= delta;
+        ref.current.rotation.z -= delta;
+        // ref.current.position.z += delta; brings it forward 
+        ref.current.position.z = Math.sin(state.clock.elapsedTime) * 0.5;
+    })
+
+
     return (
-        // position: [x, y, z] - Controls the sphere's position in 3D space
-      <mesh position={position}> 
-          <boxGeometry args={side} />
+        // Fundamental building block for rendering any building bocks. A mesh needs a germetry shape and everything within the mesh are its properties- like a container
+      <mesh ref={ref} position={position}> 
+          <boxGeometry args={args} />
           <meshStandardMaterial color={color}/> 
       </mesh>
     )
